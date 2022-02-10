dc007253525ea218eee9907dae3dad07caa3fb4b
pgg-dev <park.geumgyu@gmail.com>
2022-02-11 06:36:03 +0900

    test: flexBox layout test

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style="light" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 5, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-  }
-});
