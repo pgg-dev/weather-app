cb17bfd032aa39ac1cd04aea248538b6dd7066a0
pgg-dev <park.geumgyu@gmail.com>
2022-02-18 07:01:03 +0900

    Feat: 날씨 아이콘 적용

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import { Fontisto } from "@expo/vector-icons"
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = '7d89e8b6f3fc114e8b179b757e37bc5e'
-
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rain",
+  Thunderstorm: "lightning"
+}
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
@@ -28,7 +36,6 @@ export default function App() {
     setCity(location[0].district);
     const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
-    console.log(json)
     setDays(json.daily)
   };
 
@@ -48,12 +55,15 @@ export default function App() {
         contentContainerStyle={styles.weather}
       >
         {days.length === 0 ?
-            <View style={styles.day}>
+            <View style={{...styles.day, alignItems: "center"}}>
               <ActivityIndicator color='white' style={{ marginTop: 10 }} size='large'/>
             </View> :
             days.map((day, index) =>
                 <View key={index} style={styles.day}>
-                  <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                  <View style={{ width: "100%", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                    <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                    <Fontisto name={icons[day.weather[0].main]} size={68} color="black" />
+                  </View>
                   <Text style={styles.description}>{day.weather[0].main}</Text>
                   <Text style={styles.tinyText}>{day.weather[0].description}</Text>
                 </View>
